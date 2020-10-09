665bfb136ea56b157fbe88bf10bf31566964d0bf
Hazber <62963127+Hazber@users.noreply.github.com>
2020-10-09 23:51:39 +0300

    Простой вывод фигур
    Просто выводит заданные фигуры
    TODO:
    Рисование мышью, доработать модули, доделать кисти

--- Test1/Test1/Test1.cpp
+++ Test1/Test1/Test1.cpp
@@ -4,25 +4,15 @@
 #include "framework.h"
 #include "Figures.h"
 #include "Test1.h"
-//#include "stdafx.h"
 #include <unordered_map>
 
 #define MAX_LOADSTRING 100
 
-// Глобальные переменные:
-HINSTANCE hInst;                                // текущий экземпляр
-WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
-WCHAR szWindowClass[MAX_LOADSTRING];     
-       // имя класса главного окна
-enum Figure
-{
-    RECTANGLE,
-    LINE,
-    OVAL,
-    BROKEN,
-    TRIANGLE
 
-}wFigure;
+HINSTANCE hInst;                                
+WCHAR szTitle[MAX_LOADSTRING];                  
+WCHAR szWindowClass[MAX_LOADSTRING];     
+       
 HDC hdc;
 static HDC secondHDC;
 PAINTSTRUCT ps;
@@ -33,16 +23,11 @@ LOGFONT lf;
 HFONT hFont;
 HPEN hPen;
 HWND hwnd;
-HMENU menu;
 
-// Отправить объявления функций, включенных в этот модуль кода:
+
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-VOID InitToolsMenuItemsAssocs();
-
-//std::unique_ptr<paint::AppContext> context;
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -83,12 +68,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 }
 
 
-
-//
-//  ФУНКЦИЯ: MyRegisterClass()
-//
-//  ЦЕЛЬ: Регистрирует класс окна.
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
@@ -110,19 +89,9 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     return RegisterClassExW(&wcex);
 }
 
-//
-//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
-//
-//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
-//
-//   КОММЕНТАРИИ:
-//
-//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
-//        создается и выводится главное окно программы.
-//
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+   hInst = hInstance; 
 
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
@@ -138,16 +107,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
-//
-//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
-//
-//  WM_COMMAND  - обработать меню приложения
-//  WM_PAINT    - Отрисовка главного окна
-//  WM_DESTROY  - отправить сообщение о выходе и вернуться
-//
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
@@ -160,29 +119,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         HBITMAP bMap = CreateCompatibleBitmap(hdc, 1000, 1000);
         SelectObject(secondHDC, bMap);
     }
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Разобрать выбор в меню:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
     case WM_PAINT:
         {
             
             HDC hdc = BeginPaint(hWnd, &ps);
-           
-
+         
             hBrush = CreateHatchBrush(HS_FDIAGONAL, RGB(255, 0, 0));
             SelectObject(hdc, hBrush);
            
@@ -202,7 +143,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             TextOutW(hdc, 150, 270, L"Hello World", lstrlen(L"Hello World"));
 
             //линия
-              DrawLine(hdc, 300, 250, 300, 300);
+            DrawLine(hdc, 300, 250, 300, 300);
 
             DrawEraseHand(hdc, 500, 250, 350, 300);
            
@@ -234,33 +175,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         }
         break;
 
-  
-    case WM_KEYDOWN:
-    {
-        switch (wParam)
-        {
-        case VK_LEFT:
-            wFigure = RECTANGLE;
-            break;
-        case VK_RIGHT:
-            wFigure = OVAL;
-            break;
-
-        default:
-            break;
-        }
-    }
-    break;
-    case WM_LBUTTONDOWN:
-    {
-        switch (wFigure) 
-        {
-        case RECTANGLE:
-        {}
-            
-        }
-    }
-    break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -270,40 +184,3 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-// Обработчик сообщений для окна "О программе".
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
-}
-
-
-VOID InitToolsMenuItemsAssocs()
-{
-    menu = GetMenu(hwnd);
-
-   /* std::unordered_map<DWORD, paint::Tool> assocs;
-    assocs.insert(std::pair<DWORD, paint::Tool>(ID_TOOLBAR_PEN, paint::Tool::Pen));
-    assocs.insert(std::pair<DWORD, paint::Tool>(ID_TOOLBAR_LINE, paint::Tool::Line));
-    assocs.insert(std::pair<DWORD, paint::Tool>(ID_TOOLBAR_RECTANGLE, paint::Tool::Rectangle));
-    assocs.insert(std::pair<DWORD, paint::Tool>(ID_TOOLBAR_ELLIPSE, paint::Tool::Ellipse));
-    assocs.insert(std::pair<DWORD, paint::Tool>(ID_TOOLBAR_POLYLINE, paint::Tool::Polyline));
-    assocs.insert(std::pair<DWORD, paint::Tool>(ID_TOOLBAR_TEXT, paint::Tool::Text));
-
-    auto toolbar = context->GetToolbar();
-    toolbar->LoadMenuItemLinks(assocs);
-    toolbar->SelectTool(paint::Tool::Pen);*/
-}
